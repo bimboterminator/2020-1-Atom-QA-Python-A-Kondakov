48ab217e174162111ab240ff2a70ba684c7904d9
Kondakov Alexander <53858094+bimboterminator@users.noreply.github.com>
2020-10-28 17:51:05 +0300

    Qa hw3 Api  (#4)
    * QA-HW3
    
    * nothing special
    
    * check deleted
    
    * check deleted

---
+++ hw3/tests/test_target.py
@@ -0,0 +1,14 @@
+import pytest
+
+
+class TestTarget:
+    @pytest.mark.API
+    def test_segment_creation(self, api_client):
+        id = api_client.seg_create()
+        assert id != ''
+
+    @pytest.mark.API
+    def test_delete_segment(self, api_client):
+        id_create = api_client.seg_create()
+        resp_del = api_client.delete_segment(id_create)
+        assert not api_client.is_present(id_create)
